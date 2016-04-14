39234c25b063dac529f9823bec25f54db9afb6a4
Brian Walen <bwalen@hotmail.com>
2016-04-14 16:15:05 -0700

    Completed new exercise.

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("./public"));
+
+app.get("/user", function(req, res){
+  var user = {
+    name: "Ron",
+    location: "Newport Beach"
+  }
+  res.json(user);
+})
+
+app.get("/todos/:user", function(req,res){
+  if (req.params.user == "Ron"){
+    var todos = ["Teach Javascript.", "Go Home"];
+    res.send(todos);
+    }
+    else{
+      res.sendStatus(404);
+    }
+})
+
+app.listen(1337);
