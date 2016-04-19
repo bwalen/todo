6618cc35d8ffbcb7d28053e617c69e56057f1655
Brian Walen <bwalen@hotmail.com>
2016-04-19 16:25:22 -0700

    Deleted console.log commands and changed way of getting link elements.

--- frontend.spec.js
+++ frontend.spec.js
@@ -1,9 +1,7 @@
 var page = require('webpage').create();
-console.log("test");
 page.open('http://localhost:1337', function(status){
-  console.log("test2");
   page.evaluate(function(){
-    document.getElementById("1").click();
+    document.getElementsByTagName("a")[0].click();
   })
   setTimeout(function(){
     page.render('homepage.png');
