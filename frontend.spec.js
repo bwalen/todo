e6966a3152c63e7a5351d421fdcb0155d6710de8
Brian Walen <bwalen@hotmail.com>
2016-04-19 16:13:18 -0700

    Updated exercise one-hundred thirty to click on the ng-view link and then take screenshot.

--- frontend.spec.js
+++ frontend.spec.js
@@ -1,5 +1,10 @@
 var page = require('webpage').create();
-page.open('http://localhost:1337/#/todo', function() {
+console.log("test");
+page.open('http://localhost:1337', function(status){
+  console.log("test2");
+  page.evaluate(function(){
+    document.getElementById("1").click();
+  })
   setTimeout(function(){
     page.render('homepage.png');
     phantom.exit();
